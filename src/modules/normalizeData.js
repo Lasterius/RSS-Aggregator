dc7aab269a93c18c7d168a749c424df921ef74b0
Lasterius <zrk.vlg@gmail.com>
2023-11-13 19:35:01 +0100

    refactor project

--- src/modules/normalizeData.js
+++ src/modules/normalizeData.js
@@ -2,17 +2,17 @@
 import _ from 'lodash';
 
 export const getUniquePosts = (posts, currentPosts) => {
-	const uniquePosts = _.differenceWith(posts, currentPosts, _.isEqual);
-	return uniquePosts;
+  const uniquePosts = _.differenceWith(posts, currentPosts, _.isEqual);
+  return uniquePosts;
 };
 
 export default (data) => {
-	const { feed, posts } = data;
-	const feedId = _.get(feed, 'id', _.uniqueId('feed'));
-	const normalizedFeed = { ...feed, id: feedId };
-	const normalizedPosts = posts.map((post) => {
-		const id = _.uniqueId('post');
-		return { ...post, id, feedId };
-	});
-	return { feed: normalizedFeed, posts: normalizedPosts };
+  const { feed, posts } = data;
+  const feedId = _.get(feed, 'id', _.uniqueId('feed'));
+  const normalizedFeed = { ...feed, id: feedId };
+  const normalizedPosts = posts.map((post) => {
+    const id = _.uniqueId('post');
+    return { ...post, id, feedId };
+  });
+  return { feed: normalizedFeed, posts: normalizedPosts };
 };
